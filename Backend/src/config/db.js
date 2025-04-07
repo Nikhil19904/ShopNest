eafcd27a5d72dc65dfd45f9b4bf7eff50e5b169e
Nikhil Suryavanshi <nc429529@gmail.com>
2025-04-07 11:01:29 -0700

    Update db.js

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -5,3 +5,9 @@ const connectDb = async() =>{
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Database connected.")
 
+    }catch(error){
+        console.log(error)
+    }
+}
+
+module.exports = connectDb
